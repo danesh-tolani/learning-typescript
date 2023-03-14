dce15607bcd7e6d5f476a33fb6a5b60d62ae3287
Danesh Tolani <danesh.tolani@gmail.com>
2023-03-14 20:53:17 +0530

    learned enums

---
+++ myEnums.js
@@ -0,0 +1,10 @@
+"use strict";
+// enum SeatChoice {
+//   AISLE = "aisle",
+//   MIDDLE = "middle",
+//   WINDOW = "window",
+//   FOURTH = "fourth",
+// }
+exports.__esModule = true;
+var seat = "middle" /* SeatChoice.MIDDLE */;
+console.log(seat); // 11
---
+++ myEnums.ts
@@ -0,0 +1,18 @@
+// enum SeatChoice {
+//   AISLE = "aisle",
+//   MIDDLE = "middle",
+//   WINDOW = "window",
+//   FOURTH = "fourth",
+// }
+
+const enum SeatChoice {
+  AISLE = "aisle",
+  MIDDLE = "middle",
+  WINDOW = "window",
+  FOURTH = "fourth",
+}
+
+const seat = SeatChoice.MIDDLE;
+console.log(seat); // 11
+
+export {};
