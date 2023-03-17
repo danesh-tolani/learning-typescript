fc324d10b4ae8883eedff1a901c6220aa8d7e2ea
Danesh Tolani <100458159+danesh-tolani@users.noreply.github.com>
2023-03-17 07:54:07 +0000

    learned instanceof, discreminated union and never

--- purets/src/detections.ts
+++ purets/src/detections.ts
@@ -30,3 +30,87 @@ function isAdmin(account: User | Admin) {
     return account.isAdmin;
   }
 }
+
+
+//instanceof
+// mostly used new keyword in class, objects
+
+function logValue(x: Date | String) {
+  if(x instanceof Date) {
+    console.log(x.toUTCString())
+  }
+  else {
+    console.log(x.toUpperCase());
+  }
+}
+
+
+type Fish = {
+  swim: () => void
+}
+
+type Bird = {
+  fly: () => void
+}
+
+function isFish(pet: Fish | Bird): pet is Fish {  // if the below return is true it will return Fish else it will return Bird
+  return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird): string{
+  if(isFish(pet)) {
+    pet
+    return "fish food"
+  }
+  else {
+    pet
+    return "bird food"
+  }
+}
+
+
+
+// discreminated union
+// kind
+interface Circle {
+  kind: "circle"
+  radius: number
+}
+
+interface Square {
+  kind: "square"
+  side: number
+}
+
+interface Rectangle {
+  kind: "rectangle"
+  length: number
+  width: number
+}
+
+type Shape = Circle | Square | Rectangle
+
+function getTrueShape(shape: Shape){
+  if(shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2
+  }
+  // return shape.side * shape.side
+}
+
+
+function getArea(shape: Shape) {
+  switch(shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2
+
+    case "square":
+      return shape.side * shape.side
+    
+    case "rectangle":
+      return shape.length * shape.width
+
+    default: 
+      const _defaultForShape: never = shape
+      return _defaultForShape
+  }
+}
